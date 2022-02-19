7def074f27ccda9f0c4507658412395b5e76d0e7
claudia.davis <claudiacdavis@gmail.com>
2022-02-19 12:13:12 -0500

    array of questions and initial license markdown

--- index.js
+++ index.js
@@ -1,7 +1,101 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const propmtQuestions = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "What is your project title? (Required)",
+      validate: (titleInput) => {
+        if (titleInput) {
+          return true;
+        } else {
+          console.log("Please enter your project title!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Provide a description of the project (Required)",
+      validate: (descriptionInput) => {
+        if (descriptionInput) {
+          return true;
+        } else {
+          console.log("You need to enter a project description!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "install",
+      message:
+        "If applicable, provide installation instructions for the project",
+      default: false,
+    },
+    {
+      type: "input",
+      name: "usage",
+      message:
+        "Provide a description of the usage information for the project (Required)",
+      validate: (usageInput) => {
+        if (usageInput) {
+          return true;
+        } else {
+          console.log("You need to enter usage information!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "contribution",
+      message:
+        "Provide a description of the contributions to the project (Required)",
+      validate: (contributionInput) => {
+        if (contributionInput) {
+          return true;
+        } else {
+          console.log("You need to enter contribution information!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "question",
+      message:
+        "Provide a description of how viewers can reach you with questions about your project, including your email address (Required)",
+      validate: (questionInput) => {
+        if (questionInput) {
+          return true;
+        } else {
+          console.log("You need to enter contact information!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "confirm",
+      name: "confirmLicense",
+      message: "Would you like to add a license to your project?",
+      default: true,
+    },
+    {
+      type: "checkbox",
+      name: "licenses",
+      message: "What license did you create this project with?",
+      choices: ["Apache", "MIT", "GNU", "BSD", "ISC"],
+      when: ({ confirmLicense }) => confirmLicense,
+    },
+  ]);
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -11,3 +105,5 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+propmtQuestions();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,12 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (!license) {
+    return "";
+  } else {
+    return ``;
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -8,7 +14,15 @@ function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) {
+    return "";
+  } else {
+    return `
+    ##${license}
+    `;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
